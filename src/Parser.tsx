56ddddbf2776cc8db6f644613320eec9a921995f
ND <nimmervoll.david@gmail.com>
2021-03-25 10:16:43 +0100

    added parser

--- src/Parser.tsx
+++ src/Parser.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./Parser.css";
+var model = require("./model");
 
 interface IProps {}
 
@@ -10,9 +11,18 @@ export default class Parser extends React.Component<IProps, IState> {
         super(props);
     }
 
+    onClick() {
+        var tree = model.parse("(comp A 10)(comp B 10)(param k 0.1)(react A B {A * k})");
+        console.log(tree);
+    }
     componentDidMount() {}
 
     render() {
-        return <div className="parser">Parser</div>;
+        return (
+            <div className="parser">
+                <button onClick={this.onClick}>test</button>
+                Parser
+            </div>
+        );
     }
 }
