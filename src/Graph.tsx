b2bb3af0e777c1842c3a2766995f51a800b66a9b
ND <nimmervoll.david@gmail.com>
2021-03-29 09:58:09 +0200

    graph 1

--- src/Graph.tsx
+++ src/Graph.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import "./Graph.css";
 
+import ForceGraph2D from "react-force-graph-2d";
+
 interface IProps {}
 
 interface IState {}
@@ -13,6 +15,24 @@ export default class Graph extends React.Component<IProps, IState> {
     componentDidMount() {}
 
     render() {
-        return <div className="graph">Graph</div>;
+        return (
+            <div className="graph">
+                Graph
+                <ForceGraph2D
+                    graphData={{
+                        nodes: [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }, { id: 4 }],
+                        links: [
+                            { source: 0, target: 1 },
+                            { source: 3, target: 4 },
+                            { source: 2, target: 4 },
+                            { source: 3, target: 1 },
+                        ],
+                    }}
+                    nodeLabel="id"
+                    width={500}
+                    height={500}
+                />
+            </div>
+        );
     }
 }
