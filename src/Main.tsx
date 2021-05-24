dcf765d35b9d1dc72a09a6b0a17acdb56d480222
ND <nimmervoll.david@gmail.com>
2021-05-24 08:57:30 +0200

    added runge kutta 4

--- src/Main.tsx
+++ src/Main.tsx
@@ -53,12 +53,49 @@ export default class Main extends React.Component<IProps, IState> {
             var k1 = this.evaluateExpression(c.ODE, variables) * (this.state.stepSize / 2);
             middleVariables.set(c.name, c.value[c.value.length - 1] + k1);
         });
-        console.log(variables);
-        console.log(middleVariables);
 
         this.state.model.compartments.forEach((c) => {
             var k2 = this.evaluateExpression(c.ODE, middleVariables) * this.state.stepSize;
             c.value.push(c.value[c.value.length - 1] + k2);
+        });
+    }
+
+    //Runge-Kutta solver 4 Order
+    rungeKutta4(variables: Map<string, number>) {
+        var middleVariables = new Map(variables); //contains variables for intermediary steps
+        var allK = new Map();
+        //calc k1
+        this.state.model.compartments.forEach((c) => {
+            var k1 = this.evaluateExpression(c.ODE, variables) * this.state.stepSize;
+            middleVariables.set(c.name, c.value[c.value.length - 1] + k1 / 2);
+            allK.set(c.name, [k1]);
+        });
+        //calc k2
+        this.state.model.compartments.forEach((c) => {
+            var k2 = this.evaluateExpression(c.ODE, middleVariables) * this.state.stepSize;
+            middleVariables.set(c.name, c.value[c.value.length - 1] + k2 / 2);
+            allK.get(c.name).push(k2);
+        });
+        //calc k3
+        this.state.model.compartments.forEach((c) => {
+            var k3 = this.evaluateExpression(c.ODE, middleVariables) * this.state.stepSize;
+            middleVariables.set(c.name, c.value[c.value.length - 1] + k3);
+            allK.get(c.name).push(k3);
+        });
+        //calc k4
+        this.state.model.compartments.forEach((c) => {
+            var k4 = this.evaluateExpression(c.ODE, middleVariables) * this.state.stepSize;
+            allK.get(c.name).push(k4);
+        });
+        //calc y_n+1
+        this.state.model.compartments.forEach((c) => {
+            c.value.push(
+                c.value[c.value.length - 1] +
+                    (1 / 6) * allK.get(c.name)[0] + //k1
+                    (2 / 6) * allK.get(c.name)[1] + //k2
+                    (2 / 6) * allK.get(c.name)[2] + //k3
+                    (1 / 6) * allK.get(c.name)[3], //k4
+            );
         });
     }
 
@@ -77,7 +114,8 @@ export default class Main extends React.Component<IProps, IState> {
             this.state.model.parameters.forEach((p) => variables.set(p.name, p.value)); //TODO insert Constant at parse time
             //calculate Step
             //this.euler(variables);
-            this.rungeKutta2(variables);
+            //this.rungeKutta2(variables);
+            this.rungeKutta4(variables);
             //save Timestamps for variable step size
             this.state.timeSteps.push(this.state.timeSteps[this.state.currentTick + i] + this.state.stepSize);
         }
